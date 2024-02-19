51eeee46f97fe084f47271cc1abf132c4ef319da
Alexander Ortiz <ludwingaelxander230@gmail.com>
2024-02-19 09:43:49 -0600

    CONEXION DIFERENTES ARCHIVOS FORTRAN

--- Main.f90
+++ Main.f90
@@ -1,4 +1,10 @@
 program Main
+
+    use Menu
+    implicit none
+
     ! mensaje "hola mundo" fortran 
     print *, 'Hola Mundo!'
+
+    call Mensaje()
   end program Main
---
+++ Menu.f90
@@ -0,0 +1,17 @@
+module Menu
+    implicit none
+
+    public :: Mensaje
+    
+    contains
+
+    subroutine Mensaje()
+        print *, 'Hola mundo, pruba diferente archivo'
+    end subroutine Mensaje
+
+
+
+    
+end module Menu
+    
+
